6e7eb1fb960078b19cb948988cfa98d480e6b42e
Swarsele <32304731+Swarsel@users.noreply.github.com>
2022-03-15 16:54:31 +0100

    Leon's Assignment 1: Line 7-11
    Also fixed line 4

--- Assignment01A_GRP34.m
+++ Assignment01A_GRP34.m
@@ -1,7 +1,11 @@
 preamble
 R=rand(5,8)
 R([1 2 4],[3 5 7])
-R(2:2:size(R,1),2:2:size(R,2))
+R(2:2:size(R,1),1:2:size(R,2))
 R(R(:,1)>0.5,:)
 R(:,R(size(R, 1),:)<0.4)
 S=R
+S(S<0.2)=-S(S<0.2)
+S(S<0)=-inf
+S(S>0.9)=-nan
+find(~isinf(S) & ~isnan(S))
