54f96203c153e3e0bdcdf60f297fc2d240456554
Swarsele <leon.schwarzaeugl@gmail.com>
2022-03-18 18:19:17 +0100

    Assignment02B: Rename variables for clarity

--- Assignment01/Assignment02B_GRP34.m
+++ Assignment01/Assignment02B_GRP34.m
@@ -1,6 +1,7 @@
 preamble;
-[numbers, check] = deal(1:1:1000, 4:2:1000);
-primes = numbers(isprime(numbers))
-sumprime = sort(unique(reshape((primes+primes.')',[],1)'))
-sumprime = sumprime(sumprime <= 1000)
-assert(all(ismember(check,sumprime)))
+N = 1000;
+[numbers, check] = deal(1:1:N, 4:2:N);
+primes = numbers(isprime(numbers));
+primesums = sort(unique(reshape((primes+primes.')',[],1)'));
+primesums = primesums(primesums <= N);
+assert(all(ismember(check,primesums)))
