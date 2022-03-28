662faca3d51b39faab38d768fac17bd6dc428041
kzenz97 <101670452+kzenz97@users.noreply.github.com>
2022-03-29 00:41:32 +0200

    Add files via upload

---
+++ Assignment03/Assignment03B_GRP34.m
@@ -0,0 +1,19 @@
+preamble
+delta=1E-10;
+X=[0:0.5:10];
+N=[0:0.5:4];
+T=table();
+for alpha=N
+    k=0;
+    J=((X./2).^alpha)./(gamma(1)*gamma(1+alpha)); %first term in the sum
+    k=k+1; %execute second iteration because Jk must exist before loop
+    Jk=((X./2).^(2*k+alpha)).*((-1)^k/(gamma(k+1)*gamma(k+1+alpha)));
+    while norm(Jk, "inf")>=delta
+        J=J+Jk;
+        k=k+1;
+        Jk=((X./2).^(2*k+alpha)).*((-1)^k/(gamma(k+1)*gamma(k+1+alpha)));
+    end
+    Tj=table(num2str(J'),'VariableNames',{strcat('alpha=',num2str(alpha))});
+    T=[T Tj];
+end
+T
