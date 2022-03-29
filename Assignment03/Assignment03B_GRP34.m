422de308c4084ab04f74d4e17c98f300d6b94d40
kzenz97 <101670452+kzenz97@users.noreply.github.com>
2022-03-29 17:59:10 +0200

    Update Assignment03B_GRP34.m

--- Assignment03/Assignment03B_GRP34.m
+++ Assignment03/Assignment03B_GRP34.m
@@ -2,7 +2,7 @@ preamble
 delta=1E-10;
 X=[0:0.5:10];
 N=[0:0.5:4];
-T=table();
+T=table(X','VariableNames',{'x='});
 for alpha=N
     k=0;
     J=((X./2).^alpha)./(gamma(1)*gamma(1+alpha)); %first term in the sum
@@ -13,7 +13,7 @@ for alpha=N
         k=k+1;
         Jk=((X./2).^(2*k+alpha)).*((-1)^k/(gamma(k+1)*gamma(k+1+alpha)));
     end
-    Tj=table(num2str(J'),'VariableNames',{strcat('alpha=',num2str(alpha))});
+    Tj=table(J','VariableNames',{strcat('alpha=',num2str(alpha))});
     T=[T Tj];
 end
 T
