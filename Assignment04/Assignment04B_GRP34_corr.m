e5d1193e3f4d3a53d370527273b8eba823a75161
Swarsele <leon.schwarzaeugl@gmail.com>
2022-04-05 23:46:27 +0200

    (Hopefully) corrected Assignment04B
    only changed 27.5 to 27.4 in vector c

---
+++ Assignment04/Assignment04B_GRP34_corr.m
@@ -0,0 +1,9 @@
+preamble
+mypol=@(x,c) (sum(c(:)'.*x(:).^[length(c)-1:-1:0], 2));
+x=[0:0.5:5];
+c=[0.1,-1.5,8.5,-22.5,27.4,-12];
+p1=mypol(x,c);
+p2=mypol(x,c');
+p3=mypol(x',c);
+p4=mypol(x',c');
+array2table([x' round([p1 p2 p3 p4],4)],'VariableNames',{'x' 'p1' 'p2' 'p3' 'p4'})
