72524d91bcd507fcd0910370059b18c42025ca52
kzenz97 <101670452+kzenz97@users.noreply.github.com>
2022-04-27 12:17:49 +0200

    Update Assignment05A_GRP34.m

--- Assignment05A_GRP34.m
+++ Assignment05A_GRP34.m
@@ -15,9 +15,9 @@ check_b=(n==size(y,2));
 % Due to probably computing errors, the step size is never exactly equal
 check_c=range(s)<=10^(-14); 
 if (check_a & check_b) & check_c
-    if mod(n,2)
+    if mod(n,2) % Check for odd gridpoints
         s_vec=[1 repmat([2 4], 1, (n-3)/2) 2 1];
-        I=sum(s_vec.*y)*s(1)/3;
+        I=sum(s_vec.*y)*s(1)/3; % area calculation
         return
     else
         I=trapz(x, y);
@@ -28,28 +28,31 @@ end
 end
 
 function [A,funz,h]=AreaZeros(funh,a,b,n,nn)
-funsp=@(X, Y) (X(sign(Y(1:end-1)) ~= sign(Y(2:end))));
-x=linspace(a,b,n);
+funsp=@(X, Y) (X(sign(Y(1:end-1)) ~= sign(Y(2:end)))); % starting points
+x=linspace(a,b,n); % create grid
 h=figure(1)
 clf
 funct=plot(x,funh(x),'DisplayName','$f(x)=\sin(x^2/2)\cdot\exp(-x/5)$');
 hold on
 grid on
 plot(x,zeros(size(x)))
-A=simpson(x,funh(x));
-sts=funsp(x,funh(x));
-funz=unique(arrayfun(@(z) fzero(funh,z),[sts b]));
-plot(funz,0*funz,'o','markersize',10);
+A=simpson(x,funh(x)); % Area calculation
+sts=funsp(x,funh(x)); % starting points
+funz=unique(arrayfun(@(z) fzero(funh,z),[sts b])); % calculate zeros
+plot(funz,0*funz,'o','markersize',10); % plot zeros
 
-secx=linspace(funz(3),funz(4),nn);
-[X, Y]=meshgrid(secx,funh(secx)); 
-c(1)=trapz(diag(X),diag(X.*Y))/trapz(diag(X),diag(Y));
-c(2)=trapz(diag(Y),diag(Y.*X))/trapz(diag(Y),diag(X));
-blabel=['$Barycenter: ($' num2str(c) '$)$'];
+secx=linspace(funz(3),funz(4),nn); % grid for barycenter-area
+[X, Y]=meshgrid(secx,funh(secx)); % create meshgrid
+% create triangular matrices, integrate, obtain cumulative area arrays:
+[cax, cay] = deal(trapz(secx, triu(Y), 1), trapz(funh(secx), tril(X)', 1));
+c(1) = sum(secx.*[cax(1) diff(cax)])/cax(end); % calculate x...
+c(2) = sum(funh(secx).*[cay(1) diff(cay)])/cay(end); % and y coordinate
+blabel=['$Barycenter: ($' num2str(c) '$)$']; % plot everything...
 bary=plot(c(1),c(2),'x','markersize',10,'DisplayName',blabel);
 fill(secx,funh(secx),'cyan','FaceAlpha',0.3)
 xlabel('$x$')
 ylabel('$y$')
+title('$Zeros,\ Area\ and\ Barycenter$')
 legend([funct bary])
 hold off
 end
