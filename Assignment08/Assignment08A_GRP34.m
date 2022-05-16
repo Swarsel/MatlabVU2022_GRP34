6e347005bf0da4a5816857d01c92f20f14da2ef9
kzenz97 <101670452+kzenz97@users.noreply.github.com>
2022-05-16 23:20:27 +0200

    Scrap for 08A

--- Assignment08/Assignment08A_GRP34.m
+++ Assignment08/Assignment08A_GRP34.m
@@ -1,62 +1,25 @@
-preamble
-%
-syms y(x) % unknown symbolic function
-Dy=diff(y,x,1); % first derivative
-D2y=diff(y,x,2); % second derivative
-D3y=diff(y,x,3); % third derivative
-
-syms k q M % constant coefficients
-syms C1 C2 C3 % integrations constant
-
-eqstr="y'- k * sqrt(y) * (1 - y/M)"; % ay'+by=0
-k0=0.05; M0=10; const=[k0 M0]; % set constants
-inicond=[y(0)==5]; % initial condition
-rg=[0 1]; % plot range
-titstr="$y'- 0.05 \sqrt{y} (1- y/10) = 0,\ y(0)=5$";
-
-lastwarn('')
-figure(1)
-clf
-mystr="Equation: $"+titstr+"$";
-title(mystr,'fontsize',18)
-box on
-xlabel('$t$')
-ylabel('$y(t)$')
-%
-eqs=strrep(eqstr,"y'''","D3y");eqs=strrep(eqs,"y''","D2y");eqs=strrep(eqs,"y'","Dy");
+preamble;
+k0=0.05; q0=0.95; M0=10; x0=-300; y0=5;
+syms x k M; syms y(x) yq(x);
+eqstr="y'-k*sqrt(y)*(1-y/M)";
+a0=3;b0=2;const=[a0 b0]; 
+inicond=[y(1)==2]; 
+rg=[0 10]; 
+titstr="3y'+2y=0,\ y(1)=2";
+Dy=diff(y,x,1);
+eqs=strrep(eqstr,"y'","Dy");
 eq=eval(eqs);
-disp(['Equation : ',char(eqstr),'=0']);
-ysolgen(x)=dsolve(eq==0, 'x');
-disp(['General solution: y(x)=',char(ysolgen(x))]);
-
-% check solution by substituting ysolgen back into equation
-s=simplify(subs(eq,y,ysolgen))
-if s==0,disp('General solution is correct'),end
-% particular solution
-ysolpar(x)=simplify(dsolve(eq==0,inicond));
-disp(['Particular solution: y(x)=',char(ysolpar(x))]);
-s=simplify(subs(eq,y,ysolpar));
-if s==0,disp('Particular solution is correct'),end
-% substitute constants, if any
-if ~isempty(const)
-    v=symvar(eq);
-    eqsubs=subs(eq,v(2:end),const);
-    for j=2:length(v)
-        eqstr=strrep(eqstr,char(v(j)),num2str(const(j-1)));
-    end
-    disp('Substitute constants')
-    disp(['Equation: ',char(eqstr),'=0']);
-    ysolpar(x)=simplify(dsolve(eqsubs,inicond));
-    disp(['Particular solution: y(t)=',char(ysolpar(x))]);
-    s=simplify(subs(eqsubs,y,ysolpar));
-end
-% plot solution
-fplot(real(ysolpar),rg); % 
-title(mystr,'fontsize',20)
-legend(['$y(t)=',latex(ysolpar),'$'],'fontsize',20,'location','best')
-box on
-xlabel('$t$')
-ylabel('$y(t)$')
-grid on
-drawnow
-str=input('Type CR to continue or 0 to stop','s');
+ysg(x)=dsolve(eq==0)
+eq=subs(eq, [k M], [k0 M0]);
+ysp(x)=simplify(dsolve(eq==0,y(0)==y0))
+assume(x,'positive');
+figure(1);
+clf;
+eqd=diff(ysp(x),x,1)==0
+m=arrayfun(@(a) ~isempty(solve(a,x)), eqd);
+yq(x)=lhs(eqd(m))
+syms x real; %reset x to include negative values
+xa=solve(yq(x0)==yq(x),x);
+xa=xa(xa~=x0); %throw away redunant coordinate
+x1=round(vpa(xa),3)
+fplot(yq,[x0 double(x1)]);
